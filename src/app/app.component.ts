23483c258f648901a58101592fe2b440a1ce1e95
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-05-27 15:01:11 -0500

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string = 'Hola Mundo';
+  public counter:number = 10;
+
+  public increaseBy(value:number):void{
+    this.counter += value;
+  }
+
+  public resetCounter():void{
+    this.counter = 10;
+  }
 }
