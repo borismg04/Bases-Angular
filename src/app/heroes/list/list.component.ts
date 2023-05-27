b3bde53ca56951b1aec0de2676624765a35b9141
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-05-27 17:27:26 -0500

    ngif ngfor

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  public heroName: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Capitan America'
+  ];
 
+  public deletedHero?: string = '';
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroName.pop();
+  }
 }
